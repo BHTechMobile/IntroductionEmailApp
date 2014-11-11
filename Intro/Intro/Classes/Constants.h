ee0b1d0df4e1fa9e28003c8c1a0eb7cf4f79acc5
Nong Trung Nghia <nghianong.bhtech@gmail.com>
2014-11-11 09:37:23 +0700

    add missing files

---
+++ Intro/Intro/Classes/Constants.h
@@ -0,0 +1,9 @@
+//
+//  Constants.h
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+typedef enum {
+    IntroTypePermission,
+    IntroTypeSimple
+}IntroType;
---
+++ Intro/Intro/Classes/UIControllers/SuggestionView.m
@@ -0,0 +1,21 @@
+//
+//  SuggestionView.m
+//  Intro
+//
+//  Created by Tommy on 11/11/14.
+//  Copyright (c) 2014 BHTech Mobile. All rights reserved.
+//
+
+#import "SuggestionView.h"
+
+@implementation SuggestionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
